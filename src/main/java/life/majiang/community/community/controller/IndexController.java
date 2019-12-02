77d2037702c07814c3bfc2399b9dc1df4f981fc9
wenyongqd <wenyongqd@gmail.com>
2019-12-02 14:32:32 -0500

    add index and boot bootstrap

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -9,11 +9,10 @@ import org.springframework.web.bind.annotation.RequestParam;
  * Created ny wenyong on 2019/12/1
  */
 @Controller  //它回自动识别扫描controller类，并把它当成bean去管理，controller类：允许这个类去接受前端的请求
-public class HelloController {
+public class IndexController {
 
-    @GetMapping("/hello")
-    public String hello(@RequestParam(name = "name") String name, Model model){
-        model.addAttribute("name",name);
-        return "hello";
+    @GetMapping("/")
+    public String index(){
+        return "index";
     }
 }
