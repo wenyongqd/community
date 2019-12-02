d3781de393dd16faa35d92da579fa8e42cc80672
wenyongqd <wenyongqd@gmail.com>
2019-12-01 22:18:50 -0500

    init repo

---
+++ src/main/java/life/majiang/community/community/controller/HelloController.java
@@ -0,0 +1,19 @@
+package life.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created ny wenyong on 2019/12/1
+ */
+@Controller  //它回自动识别扫描controller类，并把它当成bean去管理，controller类：允许这个类去接受前端的请求
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
