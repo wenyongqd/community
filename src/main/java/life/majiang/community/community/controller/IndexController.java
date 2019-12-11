6dea75ec7007642a3047cf6ab851c4951bad367b
wenyongqd <wenyongqd@gmail.com>
2019-12-10 20:33:15 -0500

    update token and session

--- src/main/java/life/majiang/community/community/controller/IndexController.java
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -1,9 +1,15 @@
 package life.majiang.community.community.controller;
 
+import life.majiang.community.community.mapper.UserMapper;
+import life.majiang.community.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created ny wenyong on 2019/12/1
@@ -11,8 +17,23 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller  //它回自动识别扫描controller类，并把它当成bean去管理，controller类：允许这个类去接受前端的请求
 public class IndexController {
 
+    @Autowired
+    private UserMapper userMapper;
     @GetMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if(cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
+
         return "index";
     }
 }
