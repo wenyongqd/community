784d99ca13dd122d7b16ec289e11fd15b01caad4
wenyongqd <wenyongqd@gmail.com>
2019-12-14 23:30:32 -0500

    update publish

--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -15,6 +15,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import java.util.UUID;
 
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -54,6 +55,7 @@ public class AuthorizeController {
             user.setAccountId(String.valueOf(githubUser.getId()));
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
+            user.setAvatarUrl(githubUser.getAvatar_url());
             userMapper.intert(user);
             response.addCookie(new Cookie("token",token));
 
