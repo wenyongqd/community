1df8925419c992a23247eccffb60cb975c8b132a
wenyongqd <wenyongqd@gmail.com>
2019-12-23 22:21:44 -0500

    update login

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -14,7 +14,14 @@ function post() {
             if(response.code == 200) {
                 $("#comment_section").hide();
             } else {
-                alert(response.message)
+                if (response.code == 2003) {
+                    var isAccepted = confirm(response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=710abdc11b7699c6273d&redirect_uri=http://localhost:8080/callback&scope=user&state=1")
+                    }
+                }else  {
+                    alert(response.message)
+                }
             }
             console.log(response)
         },
